8f1c4e6c787c5978bc0560b2406c04849d609387
Mike  Alberts <mikealberts@gmail.com>
2025-11-15 17:56:02 -0500

    And again

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	outDir: './docs',
-	site: import.meta.env.DEV 
-    ? 'http://localhost:4321/'
-    : 'https://test1.ruletheweb.us/',
+	site: 'https://test1.ruletheweb.us/',
 
 	vite: {
 		plugins: [tailwindcss()],
